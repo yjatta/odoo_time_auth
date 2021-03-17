ab9755c03b8f9d0e67bbd729b2f53e2b3271967c
Yoromang Jatta <yjatta@cayorenterprises.com>
2021-03-17 10:51:50 +0000

    SEO

--- auth_time_ip/__manifest__.py
+++ auth_time_ip/__manifest__.py
@@ -1,27 +1,16 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Time and IP Authentation",
-
+    'name': "Time &IP Authentation",
     'summary': """
-        User Time and IP Authentation""",
+        User Time Authentation and IP Authentation""",
     'license': 'GPL-3',
     'description': """
         Users cannot access the system in a specified time range
     """,
-
     'author': "Yoromang Jatta <y.jatta41@gmail.com>",
-    # 'website': "http://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
     'category': 'Uncategorized',
     'version': '0.1',
-
-    # any module necessary for this one to work correctly
     'depends': ['base'],
-
-    # always loaded
     'data': [
         'security/ir.model.access.csv',
         'views/views.xml',
